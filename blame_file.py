23021b6c7032252485d4483ce0568f16e981280f
Thomas Strunz <thomas.strunz@zoho.com>
2019-03-15 23:57:21 +0100

    Fix the subprocess call it run on Linux

--- blame_file.py
+++ blame_file.py
@@ -10,12 +10,10 @@ class Blame:
             output = subprocess.check_output(
                 shlex.split(self.gitArgs),
                 stderr=subprocess.STDOUT,
-                shell=True
+                shell=False
             ).decode()
             # print(output)
             return output
         except subprocess.CalledProcessError as e:
-            #print "Failed to run '{}': ({}) {}".format(e.cmd, e.returncode, e.output.decode())
-            print("Failed to run '{0}'".format(self.gitArgs))
-
-
+            #print("Failed to run '{0}'".format(self.gitArgs))
+            print("Failed to run '{0}': ({1}) {2}".format(e.cmd, e.returncode, e.output.decode()))
